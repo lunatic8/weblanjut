b1f6d9b5f228710a8d38e7df3f5c251ab44e86fc
Acus <84680837+yagamicruzelro@users.noreply.github.com>
2023-05-25 01:58:12 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/animate.css">
+	 <!-- bootstrap core css -->
+	 <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+	 <!-- font awesome style -->
+	 <link href="css/font-awesome.min.css" rel="stylesheet"/>
+	 <!-- Custom styles for this template -->
+	 <link href="style.css" rel="stylesheet"/>
+	 <!-- responsive style -->
+	 <link href="css/responsive.css" rel="stylesheet"/>
+    <title>Cafe</title>
+   </head>
+   <body>
+   
+   <nav>
+    <ul> <?php include("page/navbar.php") ?> </ul>
+   </nav>
+	  <!-- End of header section -->
+	  <section class="main">
+		
+		</section>
+
+
+  <?php include("content.php") ?>
+
+  <?php include("page/user/keranjang.php") ?>
+
+</body>
+
+</html>
